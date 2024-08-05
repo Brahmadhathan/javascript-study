22201af1d9cb25f48a4838d21b55649219d23c31
brahmadhathan <110897049+Brahmadhathan@users.noreply.github.com>
2024-08-05 09:30:36 +0000

    arry

---
+++ mod1/array/arrays.js
@@ -0,0 +1,2 @@
+// const myArr=[0,1,2,3,4]
+// console.log(myArr[2]);
--- mod1/conversionoperation/conv.js
+++ mod1/conversionoperation/conv.js
@@ -8,7 +8,31 @@
 //  let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn)
 
- let someNumber = 33
- let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log(typeof someNumber);
+//  let someNumber = 33
+//  let stringNumber = String(someNumber)
+//  console.log(stringNumber);
+//  console.log(typeof someNumber);
+
+
+
+
+//************operations******************** 
+
+// let value =3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = "hai"
+// let str3 = str1 + str2
+// console.log(str3);
+
+let num1 , num2,num3
+
+num1=num2=num3=2+2
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
