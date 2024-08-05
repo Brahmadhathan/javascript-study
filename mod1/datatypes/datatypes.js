dc847dd8acfa944164fdb0740da9a64f5d6ba461
brahmadhathan <110897049+Brahmadhathan@users.noreply.github.com>
2024-08-05 06:55:52 +0000

    datatype &variables

---
+++ mod1/datatypes/datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all js code as newer js version
+
+//alert(3+3) we use node js not browser
+
+ let name= "hello"
+ let age = 23
+ let isLoggedIn = false
+
+ console.log(typeof null)
---
+++ mod1/variables/varbls.js
@@ -0,0 +1,16 @@
+const accountId = 1234
+let accountEmail= "hello@gmail.com"
+var accountPassword ="123455"
+accountCity="Kearla"
+let accountState;
+//accountId = 2 not allowed
+
+
+accountEmail="helop@gmail.com"
+accountPassword="2345"
+accountCity="bngloor"
+console.log(accountId);
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
