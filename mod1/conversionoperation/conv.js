a318c66c4c4850d8be383a541ba5cddae529766e
brahmadhathan <110897049+Brahmadhathan@users.noreply.github.com>
2024-08-05 07:56:53 +0000

    conversionoperation

---
+++ mod1/conversionoperation/conv.js
@@ -0,0 +1,14 @@
+// let score = "33asd"
+
+
+// let valueInnumber =Number(score)
+// console.log(valueInnumber)
+//  let isLoggedIn = 1
+
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn)
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof someNumber);
